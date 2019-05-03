86d6ea3515e98447510d0d2ab4a218d73ad9f1bc
Franck Cassedanne <franck@ouarz.net>
2019-05-03 18:58:07 +0100

    Removing docs/ folder

--- docs/fixtures/file.js
+++
@@ -1,9 +0,0 @@
-// Apt.js
-// (c) 2009-2019 Franck Cassedanne (frqnck)
-// MIT license.
-
-$(function($) { // so it only execute once fully ready ~ works across browser
-	_apt.loadedFunc = function(file, success) {
-		return "file: '" + file + "'), success: " + success;
-	};
-})
