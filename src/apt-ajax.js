3d40ab3d2e0b9002ee85e5996433f6e6abcdd778
Jack Giffin <wowzeryest@gmail.com>
2019-10-08 01:06:26 -0400

    Removed IE paradox, fixed bugs, & reduced size (#2)
    * Removed IE paradox, fixed bugs, & reduced size
    
    The use of plain `querySelectorAll` [in the core](https://github.com/frqnck/apt.js/blob/master/src/apt-core.js#L27) condemns your library to only function [in Internet Explorer version 9 and later](https://caniuse.com/#feat=queryselector) versions. Plain old `XMLHttpRequest` works just fine in [Internet Explorer 7 and later](https://caniuse.com/#feat=mdn-api_xmlhttprequest). Thus, it is a waste of space to include a fallback for unsupported browsers such as Internet Explorer 6 and earlier.
    Additionally, checking XMLHttpRequest.prototype.status to be non-zero does not work in local files in Mozilla Firefox where the status is always zero even after the request has been completed.
    Next, the third parameter to XMLHttpRequest.prototype.open is optional: if left out, then this third parameter defaults to true and the HTTP request is asynchronous.
    Next, I fixed some nasty asynchronous UB (the worst kind of Undefined Behavior) whereby the `parse` method would never get added to `xhrInstance` if there is no `callback`. One cannot make the assumption that `parse` will never be used because, on line 28, `xhrInstance` is leaked back to the parent function where it can be utilized further and potentially use the `parse` method in the future
    Note that 3xx is a very valid and very important range of successful codes. The most common is code 304 which indicates that the resource has not been changed since the last request made.
    I also want to clarify that my changes are with the goal in mind of minimal file size *regardless of performance*. Your original code and this new code both sacrifice much performance for the sake of file size.
    Also, it is better to use `response` instead of `responseText` in case if the parent function sets `xhrInstance.responseType` to something else other than text such as is seen below.
    ```javascript
    $.ajax("binaryFile.raw", function(buffer) {
        // ...
    }).responseType = "arraybuffer";
    ```
    Lastly, I just want to suggest using clearer variable names. Using short tiny variables names makes the code harder for most people to read and does not reduce the minified file size.
    
    I like the concept behind APT.js and I hope that this major rehaul of the HTTP API will make APT.js a bit smaller and a bit more reliable.
    
    * Update apt-ajax.js
    
    * Update apt-ajax.js

--- src/apt-ajax.js
+++ src/apt-ajax.js
@@ -3,53 +3,28 @@
 // MIT license.
 
 ;(function($){
-
     /**
      * Apt Ajax.
-     * @param {String}   u - URL to request.
-     * @param {Function} c - Call back function.
-     * @param {String}   m - The request Method, default GET (passing this value will cancelsend(), manual overide).
-     * @param {internal} x - Holds XMLHttpRequest()
+     * @param {String}   url - URL to request.
+     * @param {Function} callback - Call back function.
+     * @param {String}   method - The request Method, default GET (passing this value will cancelsend(), manual overide).
+     * @param {internal} xhrInstance - Holds XMLHttpRequest()
      * @static
      */
-    $.ajax = function(u, c, m, x) {
-        c = c || function(){}
+    $.ajax = function(url, callback, method, xhrInstance) {
+        xhrInstance = new XMLHttpRequest();
 
-        try {
-            // if(window.XDomainRequest) x = new XDomainRequest(); else // Explorer 8 and 9
-            x = new XMLHttpRequest();
-        } catch (e) {
-            /*
-            Failback to handle IE/Windows variations.
-            http://blogs.msdn.com/b/xmlteam/archive/2006/10/23/using-the-right-version-of-msxml-in-internet-explorer.aspx
-            https://msdn.microsoft.com/en-us/library/ie/ms535874(v=vs.85).aspx
-            Msxml2.XMLHTTP.6.0 - MSXML 6.0 since Vista+ IE7+
-            Msxml2.XMLHTTP.3.0 - MSXML 3.0 fallback, also wotk with <IE7
-            Microsoft.XMLHTTP  - ActiveX object (best avoided)
-            */
-            var a = [6, 3];
-            for (var i = 0; i < a.length; i++)
-            try {
-                x = new ActiveXObject("Msxml2.XMLHTTP." + a[i] + ".0");
-            } catch (e) {
-                continue;
-            }
-        }
-
-        x.open(m&&m.toUpperCase()||'GET', u, true);
-        x.onreadystatechange = function() {
-            x = this;
-            if( x.readyState==4 && x.status!=0 && c) { // skip if CORS issue
-                x.parse = function() {
-                    return JSON.parse(x.responseText);
-                }
-                c(x.responseText, x.status >=200 && x.status<300, x)
-            }
+        xhrInstance.open(method && method.toUpperCase()||'GET', url);
+        xhrInstance.onloadend = function(text, status) {
+            xhrInstance.parse = JSON.parse.bind(0, text = xhrInstance.response);
+            status = xhrInstance.status;
+            
+            if (callback) callback(text, !status || status > 199 && status < 400, xhrInstance);
         };
-
-        // Will on send the request when the argument `m` is not explicitly passed.
-        m||x.send();
-        return x;
-    }
-
-})(Apt)
+        
+        if (!method) xhrInstance.send(); // Send the request when the argument `method` is not explicitly passed.
+        
+        return xhrInstance;
+    };
+    
+})(Apt);
